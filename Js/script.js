709f63d4ca3ad8e921e026bec1fb169ad6fb4316
johanwibom <johan.wibom@student.tabyenskilda.se>
2023-01-26 12:50:37 +0100

    v.2

--- Js/script.js
+++ Js/script.js
@@ -7,3 +7,13 @@
 /*****  förutom griddens laytout         *****/
 /*****                                   *****/
 /*********************************************/
+
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
